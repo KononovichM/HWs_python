c2dbd7319f02c1756e50a22830f92044ed951b99
Margarita <3826210@gmail.com>
2025-02-21 15:07:49 +0100

    final changes according comments

--- HW#8.py
+++ HW#8.py
@@ -3,36 +3,48 @@
 import random
 num_massiv = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 random.shuffle(num_massiv)
-print(num_massiv)
+
 first_four = num_massiv[:4]
-print(first_four)
-# программа просит ввести число в консоль
-print("Введите четырехзначное число:")
+# uncomment this line for debug mode
+# print(first_four)
 
-# программа считывает что ввели в консоль
-input_number = input()
-letters = list(input_number)
-print(letters)
 
-# программа сравнивает то что ввели с тем что загадали
-# for переменная in  лист/массив
-Cows = 0
-Bulls = 0
-for index, letter in enumerate(letters):
-    print(index, letter)
-# print(letter in first_four)
-    if letter in first_four and index == first_four.index(letter):
-        print("bull")
-        Bulls = Bulls + 1
+def input_compare(numbers):
+    # программа просит ввести число в консоль
+    print("Введите четырехзначное число:")
 
-    else:
-        if letter in first_four:
-            print("Cow")
-            Cows = Cows + 1
+    # программа считывает что ввели в консоль
+    input_number = input()
+    letters = list(input_number)
+    print(letters)
 
-print("Cow:", Cows)
-print("Bull", Bulls)
+    # программа сравнивает то что ввели с тем что загадали
+    # for переменная in  лист/массив
+    Cows = 0
+    Bulls = 0
+    for index, letter in enumerate(letters):
+        print(index, letter)
+    # print(letter in first_four)
+        if letter in numbers and index == numbers.index(letter):
+            print("bull")
+            Bulls = Bulls + 1
+
+        else:
+            if letter in numbers:
+                print("Cow")
+                Cows = Cows + 1
+
+    print("Cow:", Cows)
+    print("Bull", Bulls)
+    return Bulls
+
+
 # программа выдает ответ
+
+while True:
+    bulls_count = input_compare(first_four)
+    if bulls_count == 4:
+        break
 
 
 # Пирамида
@@ -41,10 +53,6 @@ def print_pyramid(lines):
         spaces = " " * (lines - line_number - 1)
         stars = "*" * (2 * line_number + 1)
         print(spaces + stars)
-
-
-def suma(a, b):
-    print(a+b)
 
 
 print_pyramid(10)
