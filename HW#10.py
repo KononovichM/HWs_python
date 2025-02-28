03ec281310d684f7e818ea0f87ba23e90a1735ed
Margarita <3826210@gmail.com>
2025-02-28 01:19:13 +0100

    fixing errors with asert

--- HW#10.py
+++ HW#10.py
@@ -72,4 +72,3 @@ assert compress_string("abeehhhhhccced") == "abe2h5c3ed"
 assert compress_string("aaabbceedd") == "a3b2ce2d2"
 assert compress_string("abcde") == "abcde"
 assert compress_string("aaabbdefffff") == "a3b2def5"
-
