b95981c8b76a87536004d0ce6fbd0f7fd08bbf23
Margarita <3826210@gmail.com>
2025-03-09 23:54:29 +0100

    Homework on OOP

--- HW12-bank.py
+++ HW12-bank.py
@@ -17,16 +17,30 @@ class Bank:
         self.clients[customer_id] = {"name": name, "deposit": None}
 
     def open_deposit_account(self, client_id1, amount, years):
+        if client_id1 not in self.clients:
+            print("Клиент " + client_id1 + " не зарегистрирован")
+            return
+
         self.clients[client_id1]["deposit"] = Deposit(amount, years)
+        print("Депозит успешно открыт")
 
     def calc_interest_rate(self, client_id1):
+        if self.has_deposit(client_id1):
+            return
+
         deposit = self.clients[client_id1].get("deposit")
         return deposit.calculate() if deposit else None
 
+    def has_deposit(self, client):
+        result = self.clients[client]["deposit"] is None
+        if result:
+            print("Ошибка: У клиента " + client + " нет открытого депозита.")
+        return result
+
     def close_deposit(self, client_id1):
-        if self.clients[client_id1]["deposit"] is None:
-            print("Ошибка: У клиента нет открытого депозита.")
+        if self.has_deposit(client_id1):
             return
+
         self.clients[client_id1]["deposit"] = None
 
 
@@ -40,3 +54,9 @@ assert bank.calc_interest_rate(client_id) == \
        1104.71, "Ошибка: расчетная сумма по вкладу отличается от ожидаемой!"
 
 bank.close_deposit(client_id)
+
+client_id2 = "0002"
+bank.open_deposit_account(client_id2, 100, 5)
+bank.register_client(client_id2, "Rita")
+bank.calc_interest_rate(client_id2)
+bank.close_deposit(client_id2)
